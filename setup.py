6862262f09f6902476a7c8523a557d9470dcfd14
Ibad Rather <ibad.rather.ir@gmail.com>
2023-05-07 04:56:31 +0200

    action v2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="DeepSearchLite",
-    version="0.1.0",
+    version="0.1.1",
     author="Ibad Rather",
     author_email="ibad.rather.ir@gmail.com",
     description="A package for image similarity search",
