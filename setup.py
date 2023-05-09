b4b3f0dbf78df98579e7c5b5243aaf93cb6683df
Ibad Rather <35704690+ibadrather@users.noreply.github.com>
2023-05-09 17:54:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="DeepSearchLite",
-    version="0.2.0",
+    version="0.2.1",
     author="Ibad Rather",
     author_email="ibad.rather.ir@gmail.com",
-    description="A package for image similarity search",
+    description="A package for similarity search for any type of data. Use your own feature extractor and data loader and use this package to perform similarity search.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ibadrather/DeepSearchLite",
